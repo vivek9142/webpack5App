e3889245f2cd0f2ebe18bebff6b68597e5deb92f
vivek9142 <vm199841@gmail.com>
2022-04-28 03:11:14 +0530

    added mode of operation based on user ease - prod,dev scripts

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const elvenShieldRecipe = {
+    leatherStrips:2,
+    ironIngot:1,
+    refineMonstone:4,
+}
+
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather:1,
+    refinedMoonstone:4
+}
+
+console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
